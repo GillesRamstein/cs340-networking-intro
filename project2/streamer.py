fd783e3614c05edcfbf1ff7bc386f2c747c3865b
Gilles Ramstein <gilles.ramstein@parashift.io>
2023-04-15 20:20:12 +0200

    project2 part3: packet loss A - background listener

--- project2/streamer.py
+++ project2/streamer.py
@@ -1,5 +1,7 @@
 # do not import anything else from loss_socket besides LossyUDP
 # do not import anything else from socket except INADDR_ANY
+from concurrent.futures import ThreadPoolExecutor
+from time import sleep
 from socket import INADDR_ANY
 from struct import pack, unpack
 from typing import Iterator
@@ -19,58 +21,68 @@ def split_bytes(data_bytes: bytes, chunk_size: int) -> Iterator[bytes]:
 class Streamer:
     def __init__(self, dst_ip, dst_port,
                  src_ip=INADDR_ANY, src_port=0):
-        """Default values listen on all network interfaces, chooses a random source port,
-           and does not introduce any simulated packet loss."""
+        """
+        Default values listen on all network interfaces, chooses a random source port,
+        and does not introduce any simulated packet loss
+        """
         self.socket = LossyUDP()
         self.socket.bind((src_ip, src_port))
         self.dst_ip = dst_ip
         self.dst_port = dst_port
-        self.segment_id = 0
-        self.recv_buff = dict()
+        self.send_segment_id = 0
         self.recv_segment_id = -1
+        self.receive_buffer = dict()
+
+        self.closed = False
+        executor = ThreadPoolExecutor(max_workers=1)
+        executor.submit(self.listener)
+
+    def listener(self):
+        """
+        Receive data and feed receive_buffer in a background thread
+        """
+        while not self.closed:
+            try:
+                data, addr = self.socket.recvfrom()
+                header = unpack(">I", data[:4])
+                segment_id = header[0]
+                body = data[4:]
+                self.receive_buffer[segment_id] = body
+            except Exception as e:
+                print("listener died!\n", e)
 
     def send(self, data_bytes: bytes) -> None:
-        """Note that data_bytes can be larger than one packet."""
-        # Part1 Chunking: split data_bytes in smaller packets
+        """
+        Note that data_bytes can be larger than one packet
+        """
         for data_chunk in split_bytes(data_bytes, BODY_LENGTH):
-            # Part2 Reordering: Add header with segment id
-            header = pack(">I", self.segment_id) # 32 bit unsigned integer
+            header = pack(">I", self.send_segment_id) # 32 bit unsigned integer
             message = header + data_chunk
             self.socket.sendto(message, (self.dst_ip, self.dst_port))
-            self.segment_id += 1
+            self.send_segment_id += 1
 
     def recv(self) -> bytes:
-        """Blocks (waits) if no data is ready to be read from the connection."""
-        # Part2: reordering
-        data, addr = self.socket.recvfrom()
-
-        # separate headers and body from data
-        header = unpack(">I", data[:4])
-        segment_id = header[0]
-        body = data[4:]
-
-        # received packet is the expected next one
-        if segment_id - 1 == self.recv_segment_id:
-            self.recv_segment_id = segment_id
-            return body
-
-        # received packet is not expected next one
-        self.recv_buff[segment_id] = body
-
-        # check if received packet filled a hole in recv_buff and return
-        # all valid packets or an empty byte string if no packet is valid.
+        """
+        Blocks (waits) if no data is ready to be read from the connection
+        """
         _body = b""
-        for i in range(self.recv_segment_id + 1, max(self.recv_buff) + 1):
-            if i not in self.recv_buff:
+        if not self.receive_buffer:
+            return _body
+
+        for i in range(self.recv_segment_id + 1, max(self.receive_buffer) + 1):
+            if i not in self.receive_buffer:
                 continue
             if i - 1 != self.recv_segment_id:
                 break
             self.recv_segment_id = i
-            _body += self.recv_buff.pop(i)
+            _body += self.receive_buffer.pop(i)
         return _body
 
     def close(self) -> None:
-        """Cleans up. It should block (wait) until the Streamer is done with all
-           the necessary ACKs and retransmissions"""
-        # your code goes here, especially after you add ACKs and retransmissions.
-        pass
+        """
+        Cleans up.
+        It should block (wait) until the Streamer is done with all
+        the necessary ACKs and retransmissions
+        """
+        self.closed = True
+        self.socket.stoprecv()
